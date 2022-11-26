4ef99194d9985614df040e00b8b859bac370bad6
victoryfox1116 <victoryfox1116@gmail.com>
2022-11-27 03:29:32 +0900

    Upload for server

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM node:alpine
+# Create App Directory
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
+
+# Install Dependencies
+COPY package*.json ./
+
+RUN npm install --silent
+
+# Copy app source code
+COPY . .
+
+# Exports
+EXPOSE 3001
+
+CMD ["npm","start"]
---
+++ server/index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const finnhub = require('finnhub');
+const cors = require('cors');
+const bodyparser = require('body-parser');
+const app = express();
+
+app.use(bodyparser.json({limit: '50mb'}));
+
+const corsOptions ={
+    // origin:'http://localhost:3000',
+    origin: 'https://finnhubtest.netlify.app', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+};
+app.use(cors(corsOptions));
+
+const api_key = finnhub.ApiClient.instance.authentications['api_key'];
+api_key.apiKey = "bv4mnbf48v6qpate9n30";
+const finnhubClient = new finnhub.DefaultApi()
+
+app.post("/api", (req, res) => {
+    finnhubClient.quote(req.body.symbol, (error, data, response) => {
+        if(data == null) // Can not connect with Finnhub 
+            res.send({
+                price: -2,
+                percentChange: -2
+            });
+        else //Get Data from Finnhub
+            /* 
+                data.c == 0 then Symbol is incorrect
+                else return data
+            */
+            res.send({
+                price: data.c ? data.c : -1,
+                percentChange: data.c ? data.dp : -1,
+            });
+    });
+});
+
+
+app.use(express.static(__dirname + '/front'));
+
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/front/index.html');
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
